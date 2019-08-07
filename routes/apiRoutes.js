d45d243433d00cb2afb6d0d40adba66e8d0af07a
Rajiv <rtatineni2@gmail.com>
2019-08-06 19:21:53 -0500

    updated heroku remote

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,14 @@
 const axios = require("axios");
 const router = require("express").Router();
 
-// Sample test route with random api
+// random Route just for testing
 router.get("/recipes", (req, res) => {
     axios
       .get("http://www.recipepuppy.com/api/", { params: req.query })
       .then(({ data: { results } }) => res.json(results))
       .catch(err => res.status(422).json(err));
   });
+
 
 
 
